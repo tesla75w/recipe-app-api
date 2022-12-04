0da9b901820ccb0b8be7d1f2067d0519727c696a
milan <milanbjekic024@gmail.com>
2022-12-04 08:51:19 +0100

    Listing ingredients implemented

--- app/recipe/urls.py
+++ app/recipe/urls.py
@@ -5,6 +5,7 @@ from recipe import views
 
 router = DefaultRouter()
 router.register('tags', views.TagViewSet, basename='tag')
+router.register('ingredients', views.IngredientViewSet, basename='ingredient')
 
 app_name = 'recipe'
 
